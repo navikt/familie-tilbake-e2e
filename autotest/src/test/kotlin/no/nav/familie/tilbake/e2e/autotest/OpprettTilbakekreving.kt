fc15c0dcff9984b947791c424abf2eae488c476a
Geir Roger Moen <geir.roger.moen@nav.no>
2021-03-03 14:47:18 +0100

    Meir oppsett for e2e-tester for familie-tilbake + litt meir testing

--- autotest/src/test/kotlin/no/nav/familie/tilbake/e2e/autotest/OpprettTilbakekreving.kt
+++ autotest/src/test/kotlin/no/nav/familie/tilbake/e2e/autotest/OpprettTilbakekreving.kt
@@ -3,6 +3,7 @@ package no.nav.familie.tilbake.e2e.autotest
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.tilbakekreving.Fagsystem
 import no.nav.familie.kontrakter.felles.tilbakekreving.Faktainfo
+import no.nav.familie.kontrakter.felles.tilbakekreving.Periode
 import no.nav.familie.kontrakter.felles.tilbakekreving.Tilbakekrevingsvalg
 import no.nav.familie.kontrakter.felles.tilbakekreving.Varsel
 import no.nav.familie.kontrakter.felles.tilbakekreving.Ytelsestype
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.time.Month
 
 @SpringBootTest(classes = [ApplicationConfig::class])
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -24,23 +26,29 @@ class OpprettTilbakekreving(@Autowired private val familieTilbakeKlient: Familie
         val fagsystem = Fagsystem.BA
         val eksternFagsakId = "fs1234"
 
+        val perioder =
+                arrayListOf(
+                        Periode(fom = LocalDate.of(2020, Month.JANUARY, 1), tom = LocalDate.of(2020, Month.MARCH, 31)),
+                        Periode(fom = LocalDate.of(2020, Month.AUGUST, 1), tom = LocalDate.of(2020, Month.OCTOBER, 30)),
+                )
         val opprettRespons = familieTilbakeKlient.opprettTilbakekreving(fagsystem = fagsystem,
-                                                                      ytelsestype = Ytelsestype.BARNETRYGD,
-                                                                      eksternId = "beh12345",
-                                                                      eksternFagsakId = eksternFagsakId,
-                                                                      revurderingsvedtaksdato = LocalDate.now(),
-                                                                      personIdent = "123456000001",
-                                                                      varsel = Varsel(varseltekst = "Ytelsen må tilbakekreves",
-                                                                                      sumFeilutbetaling = BigDecimal(2222),
-                                                                                      perioder = arrayListOf()),
-                                                                      faktainfo = Faktainfo(revurderingsårsak = "Ny søknad",
-                                                                                            revurderingsresultat = "Ytelsen opphøres",
-                                                                                            Tilbakekrevingsvalg.OPPRETT_TILBAKEKREVING_MED_VARSEL,
-                                                                                            konsekvensForYtelser = setOf("Opphør av ytelsen",
-                                                                                                                         "Ytelsen redusert")));
+                                                                        ytelsestype = Ytelsestype.BARNETRYGD,
+                                                                        eksternId = "beh12345",
+                                                                        eksternFagsakId = eksternFagsakId,
+                                                                        revurderingsvedtaksdato = LocalDate.now(),
+                                                                        personIdent = "12345678901",
+                                                                        varsel = Varsel(varseltekst = "Ytelsen må tilbakekreves",
+                                                                                        sumFeilutbetaling = BigDecimal(2222),
+                                                                                        perioder = perioder),
+                                                                        faktainfo = Faktainfo(revurderingsårsak = "Ny søknad",
+                                                                                              revurderingsresultat = "Ytelsen opphøres",
+                                                                                              Tilbakekrevingsvalg.OPPRETT_TILBAKEKREVING_MED_VARSEL,
+                                                                                              konsekvensForYtelser = setOf("Opphør av ytelsen",
+                                                                                                                           "Ytelsen redusert")));
 
         assertTrue(opprettRespons.status == Ressurs.Status.SUKSESS, "Opprett behandling skulle hatt status SUKSESS")
         val behandlingId = opprettRespons.data.toString()
+        println("Har opprettet en behandling med id $behandlingId")
 
         val fagsakRespons = familieTilbakeKlient.hentFagsak(fagsystem = fagsystem, eksternFagsakId = eksternFagsakId);
         assertTrue(fagsakRespons.status == Ressurs.Status.SUKSESS, "Fagsaken skulle ha blitt opprettet")
