5c9b80e1415bd2ed19c4621d8c636a4813bba1f3
Geir Roger Moen <geir.roger.moen@nav.no>
2021-03-09 15:34:49 +0100

    Oppsett for lokalt utviklingsmiljø

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # familie-tilbake-e2e
-Autotest for familie-tilbake
+Autotestet for familie-tilbake
+
+## Oppsett lokalt utviklingsmiljø
+
+Kjør testene med spring-profil "local". Hent TILBAKE_CLIENT_SECRET frå vault og legg det inn i Environment variables for testen.
+
+
--- autotest/src/main/kotlin/no/nav/familie/tilbake/e2e/autotest/ApplicationConfig.kt
+++ autotest/src/main/kotlin/no/nav/familie/tilbake/e2e/autotest/ApplicationConfig.kt
@@ -1,12 +1,18 @@
 package no.nav.familie.tilbake.e2e.autotest
 
+import no.nav.familie.http.config.INaisProxyCustomizer
 import no.nav.familie.http.config.RestTemplateAzure
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+import org.springframework.web.client.RestTemplate
 
 @SpringBootConfiguration
 @EntityScan(ApplicationConfig.pakkenavn)
@@ -18,5 +24,18 @@ class ApplicationConfig {
 
     companion object {
         const val pakkenavn = "no.nav.familie.tilbake.e2e"
+    }
+
+    @Bean
+    @Profile("local")
+    @Primary
+    fun restTemplateBuilder(): RestTemplateBuilder {
+        return RestTemplateBuilder(LocalINaisProxyCustomiser())
+    }
+
+    class LocalINaisProxyCustomiser: INaisProxyCustomizer {
+        override fun customize(restTemplate: RestTemplate?) {
+            // should do nothing!!!!!!1
+        }
     }
 }
